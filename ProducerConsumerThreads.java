149604c1af210d7a0d7fe10c368d336aa9a029c9
Yogesh Kumar <kumaryogesh2501@gmail.com>
2018-10-09 12:39:04 +0530

    Add files via upload

---
+++ ProducerConsumerThreads.java
@@ -0,0 +1,90 @@
+package producerconsumerthreads;
+import java.util.*;
+public class ProducerConsumerThreads {
+    public static void main(String[] args) {
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        Thread ob1 =  new Thread(new Producer(b));
+        Thread ob2 =  new Thread(new Consumer(b));
+        ob1.start();
+        ob2.start();
+    }
+}
+class Producer implements Runnable
+{
+    List<Integer> b = null;
+    final int maxsize=5;
+    int i = 0;
+    Producer(List<Integer> b)
+    {
+        this.b = b;
+    }
+    public void produce(int i) throws InterruptedException
+    {
+        synchronized(b)
+                {
+                    while(b.size()==maxsize)
+                    {
+                        System.out.println("Producer Waiting");
+                        b.wait();
+                    }    
+                }
+        synchronized(b)
+        {
+            b.add(i);
+            System.out.println("Producing...");
+       Thread.sleep(1000);
+            b.notify();
+        }
+    }
+    public void run()
+    {
+        while(true)
+        {
+            i++;
+            try {
+                produce(i);
+            } catch (InterruptedException ex) {
+             
+            }
+        }
+    }
+}
+class Consumer implements Runnable
+{
+    List<Integer> b;
+    
+    Consumer(List<Integer> b)
+    {
+         this.b = b;
+    }
+     public void consume() throws InterruptedException
+    {
+        synchronized(b)
+                {
+                    while(b.isEmpty())
+                    {
+                        System.out.println("Consumer Waiting");
+                        b.wait();
+                    }    
+                }
+        synchronized(b)
+        {
+            b.remove(0);
+            System.out.println("Consuming...");
+            Thread.sleep(1000);
+            b.notify();
+        }
+    }
+    public void run()
+    {
+        while(true)
+        {
+            
+            try {
+                consume();
+            } catch (InterruptedException ex) {
+             
+            }
+        }
+    }
+}
